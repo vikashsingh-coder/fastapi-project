af284b97b095f10e4b01486b836930538bae3a08
mentorvikash <mentorvikash@gmail.com>
2026-01-31 12:02:44 +0530

    Refactor header handling by introducing MultiHeadersModel and updating endpoint definitions for better structure and flexibility

--- Practice/HeaderParameters.py
+++ Practice/HeaderParameters.py
@@ -8,17 +8,32 @@ class HeadersModel(BaseModel):
     user_agent: str | None = None
     
 
+class MultiHeadersModel(BaseModel):
+    # model_config = {"extra": "forbid"}  # to forbid extra headers
+    model_config = {"extra": "allow"} # to allow extra headers
+
+    host: str
+    save_date: bool
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = []
+    
 # read headers example
-@app.get("/read-headers/", )
+@app.get("/read-headers/", tags=["Headers"])
 async def read_headers(user_agent: Annotated[str | None, Header(title="Pass Header", description="User are able to send header values")] = None):
     return {"user_agent": user_agent}
 
 
-@app.get("/convert_underscores-disable-headers/")
+@app.get("/convert_underscores-disable-headers/", tags=["Headers"])
 async def read_headers(strange_header: Annotated[str | None, Header(convert_underscores=False)] = None):
     return {"strange_header": strange_header}
 
 # Duplicate headers
-@app.get("/duplicate-headers/")
+@app.get("/duplicate-headers/", tags=["Headers"])
 def read_duplicate_headers(X_Token: Annotated[list[str]| None, Header()] = None):
     return {"X_token": X_Token}
+
+# multiple headers using pydantic model
+@app.get("/multiple-headers/", tags=["Headers"])
+def read_multiple_headers(headers: Annotated[MultiHeadersModel, Header()] ):
+    return {"headers": headers}
