72788a682ae0e5856554fc0cf5853fd9412f132d
mentorvikash <mentorvikash@gmail.com>
2026-02-14 23:30:11 +0530

    add metadata and licensing information to FastAPI app configuration

--- main.py
+++ main.py
@@ -19,6 +19,18 @@ You will be able to:
 app = FastAPI(
     title="ChimichangApp",
     description=description,
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "identifier": "Apache-2.0",
+    },
 )
 
 
